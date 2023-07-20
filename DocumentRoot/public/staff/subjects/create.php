5cf5bf2e52a0d917a913aaa5454ee161e79f3cfd
Kurtis Berger <118536145+milkywaysquirreldeer@users.noreply.github.com>
2023-07-20 07:33:02 -0400

    Detect form submission

--- DocumentRoot/public/staff/subjects/create.php
+++ DocumentRoot/public/staff/subjects/create.php
@@ -1,12 +1,22 @@
 <?php
 
-// Handle form values POSTed by new.php
+require_once('../../../private/initialize.php');
 
-$menuName   = $_POST['menuName'] ?? '';
-$position   = $_POST['position'] ?? '';
-$visible    = $_POST['visible'] ?? '';
+switch($_SERVER['REQUEST_METHOD']) {
+    case 'POST':
+    // Handle form values POSTed by new.php
+        $menuName   = $_POST['menuName']    ?? '';
+        $position   = $_POST['position']    ?? '';
+        $visible    = $_POST['visible']     ?? '';
 
-echo 'Form parameters<br>';
-echo 'Menu name: ' . $menuName . '<br>';
-echo 'Position: ' . $position . '<br>';
-echo 'Visible: ' . $visible . '<br>';
+        echo 'Form parameters<br>';
+        echo 'Menu name: ' . $menuName . '<br>';
+        echo 'Position: ' . $position . '<br>';
+        echo 'Visible: ' . $visible . '<br>';
+        break;
+    case 'GET':
+    // Redirect to the form that provides the expected POST data
+        redirect('/staff/subjects/new.php');
+        break;
+}
+
